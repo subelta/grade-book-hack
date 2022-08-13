8c4560d989ab3510f23de35ba68df9290233591e
Alexander Frolov <afrolovbox@gmail.com>
2022-08-13 17:44:53 +0300

    Change repo structure

--- app/config/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-env = os.environ.get('ENV')
-if env == 'production':
-    from app.config.prod import *
-else:
-    from app.config.dev import *
--- app/dependencies.py
+++
@@ -1,9 +0,0 @@
-from app.db import SessionLocal
-
-
-def get_session():
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
