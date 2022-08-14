082afd7660639a46959403c38d89ef41e64b4486
subelta <awaspecial10@gmail.com>
2022-08-14 15:24:54 +0300

    more understandable chart

--- frontend/src/components/App/SubjectsChart/SubjectsChart.js
+++ frontend/src/components/App/SubjectsChart/SubjectsChart.js
@@ -1,9 +1,9 @@
 // STFU BITCH YOU MOTHERFUCKING SHIT
 // noinspection JSValidateTypes
-
 import { Fragment, useCallback, useEffect, useState } from 'react';
-import { VictoryChart, VictoryLine, VictoryScatter, VictoryZoomContainer } from 'victory';
+import { VictoryAxis, VictoryChart, VictoryLine, VictoryScatter, VictoryZoomContainer } from 'victory';
 
+import './SubjectsChart.css';
 import { COLORS } from '../../../constants';
 
 export const SubjectsChart = (props) => {
@@ -16,58 +16,72 @@ export const SubjectsChart = (props) => {
 	}, [])
 
 	return (
-		<VictoryChart
-			style={{
-				parent: { height: "calc(100vh - 20px - 80px - 170px)", width: "100%" }
-			}}
-			containerComponent={
-				<VictoryZoomContainer
-					zoomDimension={"x"}
-					zoomDomain={zoomDomain}
-					onZoomDomainChange={handleZoom}
+		<div className={"chartWrapper"}>
+			<VictoryChart
+				style={{
+					parent: { height: "calc(100vh - 20px - 80px - 170px)", width: "1000px" }
+				}}
+				containerComponent={
+					<VictoryZoomContainer
+						zoomDimension={"x"}
+						zoomDomain={zoomDomain}
+						onZoomDomainChange={handleZoom}
+					/>
+				}
+			>
+				<VictoryAxis
+					dependentAxis
 				/>
-			}
-		>
-			{students
-				.find(el => el.user === currentStudent)
-				.subjects
-				.map(subject => (
-					<VictoryLine
-						key={subject.subj_name}
-						data={subject.dates.map(day => (
-							{
-								x: new Date(day.date),
-								y: day.grade || null
-							}
-						))}
-						style={{
-							data: {
-								stroke: COLORS[subject.subj_name],
-								strokeWidth: ({ active }) => active ? 4 : 2
-							},
-							labels: { fill: COLORS[subject.subj_name] }
-						}}
-					/>
-				))
-			}
+				<VictoryAxis
+					tickFormat={(x) =>
+						`${(new Date(x)).toLocaleDateString('de-DE', { year: 'numeric', month: 'numeric', day: 'numeric' })}`
+					}
+				/>
+				{students
+					.find(el => el.user === currentStudent)
+					.subjects
+					.map(subject => (
+						<VictoryLine
+							key={subject.subj_name}
+							data={subject.dates
+								.filter(day => !!day.grade)
+								.map(day => (
+									{
+										x: new Date(day.date),
+										y: day.grade || null
+									}
+							))}
+							style={{
+								data: {
+									stroke: COLORS[subject.subj_name],
+									strokeWidth: ({ active }) => active ? 2 : 1
+								},
+								labels: { fill: COLORS[subject.subj_name] }
+							}}
+						/>
+					))
+				}
 
-			{students
-				.find(el => el.user === currentStudent)
-				.subjects
-				.map(subject => (
-					<VictoryScatter
-						key={subject.subj_name}
-						data={subject.dates.map(day => (
-							{
-								x: new Date(day.date),
-								y: day.grade || null
-							}
-						))}
-						size={3}
-						style={{ data: { fill: COLORS[subject.subj_name] } }}
-					/>
-				))
-			}
-		</VictoryChart>
+				{students
+					.find(el => el.user === currentStudent)
+					.subjects
+					.map(subject => (
+						<VictoryScatter
+							key={subject.subj_name}
+							data={subject.dates
+								.filter(day => !!day.grade)
+								.map(day => (
+									{
+										x: new Date(day.date),
+										y: day.grade || null
+									}
+							))}
+							size={2}
+							style={{ data: { fill: COLORS[subject.subj_name] } }}
+						/>
+					))
+				}
+			</VictoryChart>
+		</div>
 	)
 }
